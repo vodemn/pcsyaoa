7f7a275f3cc19076de081587b753d45a832432aa
Vadim <44135514+vodemn@users.noreply.github.com>
2020-12-03 02:53:53 +0300

    Update generate_matrix.dart

--- course/lib/generate_matrix.dart
+++ course/lib/generate_matrix.dart
@@ -1,6 +1,20 @@
+import 'dart:io';
 import 'dart:math';
 
-void main() {
+void main() async {
+  String result = '';
+  result += _name('a') + _fillString() + ';';
+  result += '\n';
+  result += _name('b') + _fillString() + ';';
+  final filename = 'matrix.txt';
+  File(filename).writeAsString(result);
+}
+
+String _name(String name) {
+  return 'SIGNAL $name : MATRIX(1 TO M_SIZE, 1 TO M_SIZE) := ';
+}
+
+String _fillString() {
   String output = '';
   int size = 4;
   Random rand = new Random();
@@ -8,12 +22,15 @@ void main() {
   for (int i = 1; i <= size; i++) {
     output += '(';
     for (int j = 1; j <= size; j++) {
-      output += rand.nextInt(100).toString();
+      output += (rand.nextInt(10) + 1).toString();
       if (j < size) output += ', ';
     }
     output += ')';
     if (i < size) output += ', ';
   }
   output += ')';
-  print(output);
+  return output;
 }
+
+//    SIGNAL a : MATRIX(1 TO M_SIZE, 1 TO M_SIZE) := ((6, 7, 1, 7), (10, 1, 2, 2), (10, 9, 9, 7), (1, 2, 7, 2));
+//    SIGNAL b : MATRIX(1 TO M_SIZE, 1 TO M_SIZE) := ((6, 2, 1, 5), (4, 7, 1, 8), (4, 6, 7, 1), (7, 4, 1, 8));
